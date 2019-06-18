9ba00c0bf6cb29c3183a6631c2f8c4979d029862
Ramsey Chapin <ramsey@ramseychapin.com>
2019-06-18 02:05:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,16 +2,16 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  def initialize(song)
-    @song = song
-  end
-
   def self.all
     @@all
   end
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+
   end
 
 end
